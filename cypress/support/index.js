9e4e8bb9f4c803cea3ec4d44feeeac4fb9dd9f6f
Francisco Iglesias <franciscoj.iglesias@hotmail.com>
2020-06-05 14:56:38 -0300

    Initial draft of bootstrap

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -14,7 +14,10 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './commands'
+
+import '@applitools/eyes.cypress/commands';
+
+import './commands';
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
