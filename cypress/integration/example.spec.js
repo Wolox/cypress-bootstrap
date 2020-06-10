75ac20d0e9af8531d53a482f58d8448b034948cf
Edwin Gomez <edwin.gomez@wolox.co>
2020-06-10 17:10:12 -0500

    Practice signin

--- cypress/integration/example.spec.js
+++ cypress/integration/example.spec.js
@@ -1,12 +1,21 @@
-import Example from '../page-objects/example';
-
 describe('Example Test', () => {
-  const controller = new Example();
   beforeEach(() => {
-    controller.navigate('https://example.cypress.io');
+    // controller.navigate('https://example.cypress.io');
   });
 
-  it.only('finds the content "type"', () => {
-    cy.contains('type');
+  it('finds the content "type"', () => {
+    cy.visit('http://automationpractice.com/index.php');
+    cy.get('.login').click();
+    cy.get('#email_create').type('a@ae.com');
+    cy.get('#SubmitCreate').click();
+    cy.get('[name=company]').type('Wolox');
+    cy.get('#id_gender2').check();
+    cy.get('#customer_firstname').type('Edwin');
+    cy.get('#customer_lastname').type('Gomez');
+    cy.get('#passwd').type('holaMund*');
+    cy.get('#firstname').type('First Name');
+    cy.get('#lastname').type('Last Name');
+    cy.get('#address1').type('Address address 123');
+    cy.get('#city').type('Medellin');
   });
 });
